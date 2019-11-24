dbe381f3053d4d37989befa80ae96382fba4be8a
Pacôme <pacome.lehoux@laposte.net>
2019-11-24 15:27:17 +0100

    added WorkerThread class, a Thread that can handle a single request and send back a response

---
+++ src/main/java/fr/insa/http/WorkerThread.java
@@ -0,0 +1,43 @@
+package fr.insa.http;
+
+import fr.insa.http.messages.HTTPRequest;
+import fr.insa.http.messages.HTTPResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class WorkerThread extends Thread {
+    private static final Logger LOGGER = LogManager.getLogger(WorkerThread.class);
+
+    private static int workerCount = 0;
+
+    private Socket socket;
+
+    private RequestHandler requestHandler;
+
+    public WorkerThread(Socket socket, RequestHandler requestHandler) {
+        super("WorkerThread-" + workerCount++);
+        this.socket = socket;
+        this.requestHandler = requestHandler;
+    }
+
+    @Override
+    public void run() {
+        try {
+            HTTPRequest request = new HTTPRequest();
+            request.fromInputStream(this.socket.getInputStream());
+
+            LOGGER.info("Received request : {} {}", request.getMethod(), request.getResource());
+
+            HTTPResponse response = this.requestHandler.handleRequest(request);
+            response.toOutputStream(this.socket.getOutputStream());
+
+            this.socket.close();
+        }
+        catch(IOException e) {
+            LOGGER.error("An error occurred while handling request", e);
+        }
+    }
+}
