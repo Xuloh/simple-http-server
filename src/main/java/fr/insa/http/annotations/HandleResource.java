810d9a828ecc908ae20bf845c33ebf0b41bfbeec
Pacôme <pacome.lehoux@laposte.net>
2019-11-24 17:21:52 +0100

    Added 2 attributes to HandleResource
    - name to specify an exact resource name
    - regex to specify which resource match or not

--- src/main/java/fr/insa/http/annotations/HandleResource.java
+++ src/main/java/fr/insa/http/annotations/HandleResource.java
@@ -8,5 +8,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface HandleResource {
-    String value();
+    String name();
+    String regex();
 }
