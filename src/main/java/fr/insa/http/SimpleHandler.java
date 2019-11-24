2e8d7569139d46e4ef5855d1ad280edd6fa4735b
Pacôme <pacome.lehoux@laposte.net>
2019-11-24 17:24:26 +0100

    Implemented basic request handler that just says ok to everything

---
+++ src/main/java/fr/insa/http/SimpleHandler.java
@@ -0,0 +1,20 @@
+package fr.insa.http;
+
+import fr.insa.http.annotations.HTTPHandler;
+import fr.insa.http.annotations.HandleMethod;
+import fr.insa.http.enums.HTTPMethod;
+import fr.insa.http.enums.HTTPStatus;
+import fr.insa.http.messages.HTTPRequest;
+import fr.insa.http.messages.HTTPResponse;
+
+@HTTPHandler
+public class SimpleHandler {
+
+    @HandleMethod(HTTPMethod.GET)
+    public HTTPResponse handleGet(HTTPRequest request) {
+        HTTPResponse response = new HTTPResponse(HTTPStatus.OK);
+        response.setBody("<h1>It works !</h1>");
+        throw new NullPointerException("lol");
+//        return response;
+    }
+}
