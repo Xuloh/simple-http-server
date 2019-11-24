56a1448b0ba433ed2d5d1b73cea21bfac7915d54
Pacôme <pacome.lehoux@laposte.net>
2019-11-24 15:05:14 +0100

    added enum for http statuses

---
+++ src/main/java/fr/insa/http/util/Status.java
@@ -0,0 +1,203 @@
+package fr.insa.http.util;
+
+public enum Status {
+    // Information responses
+    CONTINUE(100, "Continue"),
+    SWITCHING_PROTCOL(101, "Switching Protocol"),
+    EARLY_HINTS(103, "Early Hints"),
+
+    // Successful responses
+    OK(200, "OK"),
+    CREATED(201, "Created"),
+    ACCEPTED(202, "Accepted"),
+    NON_AUTHORITATIVE_INFORMATION(203, "Non-Authoritative Information"),
+    NO_CONTENT(204, "No Content"),
+    RESET_CONTENT(205, "Reset Content"),
+    PARTIAL_CONTENT(206, "Partial Content"),
+
+    // Redirection messages
+    MULTIPLE_CHOICE(300, "Multiple Choice"),
+    MOVED_PERMANENTLY(301, "Moved Permanently"),
+    FOUND(302, "Found"),
+    SEE_OTHER(303, "See Other"),
+    NOT_MODIFIED(304, "Not Modified"),
+    TEMPORARY_REDIRECTION(307, "Temporary Redirection"),
+    PERMANENT_REDIRECTION(308, "Permanent Redirection"),
+
+    // Client error responses
+    BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    NOT_ACCEPTABLE(406, "Not Acceptable"),
+    PROXY_AUTHENTICATION_REQUIRED(407, "Proxy Authentication Required"),
+    REQUEST_TIMEOUT(408, "Request Timeout"),
+    CONFLICT(409, "Conflict"),
+    GONE(410, "Gone"),
+    LENGTH_REQUIRED(411, "Length Required"),
+    PRECONDITION_FAILED(412, "Precondition Failed"),
+    PAYLOAD_TOO_LARGE(413, "Payload Too Large"),
+    URI_TOO_LONG(414, "URI Too Long"),
+    UNSUPPORTED_MEDIA_TYPE(415, "Unsupported Media Type"),
+    REQUESTED_RANGE_NOT_SATIFIABLE(416, "Requested Range Not Satisfiable"),
+    EXPECTATION_FAILED(417, "Expectation Failed"),
+    IM_A_TEAPOT(418, "I'm a teapot"),
+    TOO_EARLY(425, "Too early"),
+    UPGRADE_REQUIRED(426, "Upgrade Required"),
+    PRECONDITION_REQUIRED(428, "Precondition Required"),
+    TOO_MANY_REQUESTS(429, "Too Many Requests"),
+    REQUEST_HEADER_FIELDS_TOO_LARGE(431, "Request Header Fields Too Large"),
+    UNAVAILABLE_FOR_LEGAL_REASONS(451, "Unavailable For Legal Reasons"),
+
+    // Server error responses
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    NOT_IMPLEMENTED(501, "Not Implemented"),
+    BAD_GATEWAY(502, "Bad Gateway"),
+    SERVICE_UNAVAILABLE(503, "Service Unavailable"),
+    GATEWAY_TIMEOUT(504, "Gateway Timeout"),
+    HTTP_VERSION_NOT_SUPPORTED(505, "HTTP Version Not Supported"),
+    VARIANT_ALSO_NEGOTIATES(506, "Variant Also Negotiates"),
+    NETWORK_AUTHENTICATION_REQUIRED(511, "Network Authentication Required");
+
+    private int statusCode;
+
+    private String statusMessage;
+
+    Status(int statusCode, String statusMessage) {
+        this.statusCode = statusCode;
+        this.statusMessage = statusMessage;
+    }
+
+    public int getCode() {
+        return this.statusCode;
+    }
+
+    public String getMessage() {
+        return this.statusMessage;
+    }
+
+    @Override
+    public String toString() {
+        return this.statusCode + ' ' + this.statusMessage;
+    }
+
+    public static Status fromCode(int statusCode) {
+        switch(statusCode) {
+            case 100:
+                return CONTINUE;
+            case 101:
+                return SWITCHING_PROTCOL;
+            case 103:
+                return EARLY_HINTS;
+            case 200:
+                return OK;
+            case 201:
+                return CREATED;
+            case 202:
+                return ACCEPTED;
+            case 203:
+                return NON_AUTHORITATIVE_INFORMATION;
+            case 204:
+                return NO_CONTENT;
+            case 205:
+                return RESET_CONTENT;
+            case 206:
+                return PARTIAL_CONTENT;
+            case 300:
+                return MULTIPLE_CHOICE;
+            case 301:
+                return MOVED_PERMANENTLY;
+            case 302:
+                return FOUND;
+            case 303:
+                return SEE_OTHER;
+            case 304:
+                return NOT_MODIFIED;
+            case 307:
+                return TEMPORARY_REDIRECTION;
+            case 308:
+                return PERMANENT_REDIRECTION;
+            case 400:
+                return BAD_REQUEST;
+            case 401:
+                return UNAUTHORIZED;
+            case 403:
+                return FORBIDDEN;
+            case 404:
+                return NOT_FOUND;
+            case 405:
+                return METHOD_NOT_ALLOWED;
+            case 406:
+                return NOT_ACCEPTABLE;
+            case 407:
+                return PROXY_AUTHENTICATION_REQUIRED;
+            case 408:
+                return REQUEST_TIMEOUT;
+            case 409:
+                return CONFLICT;
+            case 410:
+                return GONE;
+            case 411:
+                return LENGTH_REQUIRED;
+            case 412:
+                return PRECONDITION_FAILED;
+            case 413:
+                return PAYLOAD_TOO_LARGE;
+            case 414:
+                return URI_TOO_LONG;
+            case 415:
+                return UNSUPPORTED_MEDIA_TYPE;
+            case 416:
+                return REQUESTED_RANGE_NOT_SATIFIABLE;
+            case 417:
+                return EXPECTATION_FAILED;
+            case 418:
+                return IM_A_TEAPOT;
+            case 425:
+                return TOO_EARLY;
+            case 426:
+                return UPGRADE_REQUIRED;
+            case 428:
+                return PRECONDITION_REQUIRED;
+            case 429:
+                return TOO_MANY_REQUESTS;
+            case 431:
+                return REQUEST_HEADER_FIELDS_TOO_LARGE;
+            case 451:
+                return UNAVAILABLE_FOR_LEGAL_REASONS;
+            case 500:
+                return INTERNAL_SERVER_ERROR;
+            case 501:
+                return NOT_IMPLEMENTED;
+            case 502:
+                return BAD_GATEWAY;
+            case 503:
+                return SERVICE_UNAVAILABLE;
+            case 504:
+                return GATEWAY_TIMEOUT;
+            case 505:
+                return HTTP_VERSION_NOT_SUPPORTED;
+            case 506:
+                return VARIANT_ALSO_NEGOTIATES;
+            case 511:
+                return NETWORK_AUTHENTICATION_REQUIRED;
+            default:
+                throw new IllegalArgumentException("Invalid HTTP status code : " + statusCode);
+        }
+    }
+
+    public static Status fromString(String string) {
+        String[] split = string.split(" ", 2);
+        try {
+            int code = Integer.parseInt(split[0]);
+            Status status = fromCode(code);
+            if(!status.statusMessage.equals(split[1]))
+                throw new IllegalArgumentException("Invalid status message : " + split[1]);
+            return status;
+        }
+        catch(NumberFormatException e) {
+            throw new IllegalArgumentException("string must have format '<status code> <status message>'");
+        }
+    }
+}
