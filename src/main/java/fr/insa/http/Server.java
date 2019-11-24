f44e6e3b7ed07bfffb9ab21aad518be5b52359fb
Pacôme <pacome.lehoux@laposte.net>
2019-11-24 17:24:13 +0100

    Implemented basic multi-threaded server

--- src/main/java/fr/insa/http/Server.java
+++ src/main/java/fr/insa/http/Server.java
@@ -1,8 +1,28 @@
 package fr.insa.http;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private static final Logger LOGGER = LogManager.getLogger(Server.class);
 
     public static void main(String[] args) {
-        System.out.println("Hello World !");
+        int port = 8080;
+        LOGGER.info("Starting server on port {}", port);
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            FrameworkRequestHandler requestHandler = new FrameworkRequestHandler();
+            while(true) {
+                Socket socket = serverSocket.accept();
+                new WorkerThread(socket, requestHandler).start();
+            }
+        }
+        catch(IOException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
     }
 }
