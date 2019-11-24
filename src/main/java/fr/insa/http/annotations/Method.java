73274f250b9bd17c36ec875b881b936e92178247
Pacôme <pacome.lehoux@laposte.net>
2019-11-24 15:32:26 +0100

    added 2 annotations
    they describe what requests an annotated method can respond to

---
+++ src/main/java/fr/insa/http/annotations/Method.java
@@ -0,0 +1,14 @@
+package fr.insa.http.annotations;
+
+import fr.insa.http.enums.HTTPMethod;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Method {
+    HTTPMethod method();
+}
