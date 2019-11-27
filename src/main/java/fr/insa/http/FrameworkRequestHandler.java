2ca5a8fc7000dc1168c6788a34d5525439ac466e
Pacôme <pacome.lehoux@laposte.net>
2019-11-27 17:55:15 +0100

    removed unused import

--- src/main/java/fr/insa/http/FrameworkRequestHandler.java
+++ src/main/java/fr/insa/http/FrameworkRequestHandler.java
@@ -16,7 +16,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.EnumMap;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
